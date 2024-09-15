9c4e0f16fa89875a7a33d06ff19e6522401a8d26
melissa <melissa>
2024-09-15 17:15:31 +1200

    hello.py finish

--- hello.py
+++ hello.py
@@ -1,6 +1,15 @@
 # ask user's name
-name = input("What's your name?     ")
+name = input("What's your name? ").strip().title()
+
+# remove whitespace from str and capitalize user's name (each word will be titlecased)
+#name = name.strip().title()
+
+# split user's name into first and second name
+first, last = name.split(" ")
 
 # say hello to user
-print("hello ," + name)
+print(f"hello, {first}")
+# print ("hello, " + name)
+
+
 
